e7a0e438b46da4161ce3972e155b2d87c8b12cb5
xdestris <kosinski.jakub01@gmail.com>
2022-11-27 23:38:27 +0100

    add value_objects

--- lib/domain/auth/value_objects.dart
+++ lib/domain/auth/value_objects.dart
@@ -1,1 +1,26 @@
+import 'package:domain_driven_design_firebase/domain/core/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:domain_driven_design_firebase/domain/core/value_objects.dart';
+import 'package:domain_driven_design_firebase/domain/core/value_validators.dart';
 
+class EmailAddress extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory EmailAddress(String input) {
+    return EmailAddress._(validateEmailAddress(input));
+  }
+
+  const EmailAddress._(this.value);
+}
+
+class Password extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory Password(String input) {
+    return Password._(validatePassword(input));
+  }
+
+  const Password._(this.value);
+}
---
+++ lib/domain/core/value_objects.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:domain_driven_design_firebase/domain/core/failures.dart';
+
+abstract class ValueObject<T> {
+  const ValueObject();
+  Either<ValueFailure<T>, T> get value;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is ValueObject<T> && other.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  String toString() => 'Value($value)';
+}
