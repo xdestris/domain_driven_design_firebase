7cdf7494cbd87c51b8e68dd4e2f51d1a2814e1bf
xdestris <kosinski.jakub01@gmail.com>
2022-11-29 00:01:34 +0100

    implement bloc logic

--- lib/application/bloc/sign_in_form_bloc.dart
+++ lib/application/bloc/sign_in_form_bloc.dart
@@ -14,7 +14,87 @@ class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
 
   SignInFormBloc(this._iAuthFacade) : super(SignInFormState.initial()) {
     on<SignInFormEvent>((event, emit) {
-      // TODO: implement event handler
+      event.map(
+        emailChanged: (e) {
+          emit(
+            state.copyWith(
+              emailAddress: EmailAddress(e.emailStr),
+              authFailureOrSuccessOption: none(),
+            ),
+          );
+        },
+        passwordChanged: (e) {
+          emit(
+            state.copyWith(
+              password: Password(e.passwordStr),
+              authFailureOrSuccessOption: none(),
+            ),
+          );
+        },
+        registerWithEmailAndPasswordPressed: (e) async {
+          Either<AuthFailure, Unit>? failureOrSuccess;
+          final isEmailValid = state.emailAddress.isValid();
+          final isPasswordValid = state.password.isValid();
+          if (isEmailValid && isPasswordValid) {
+            emit(
+              state.copyWith(
+                isSubmitting: true,
+                authFailureOrSuccessOption: none(),
+              ),
+            );
+            failureOrSuccess = await _iAuthFacade.registerWithEmailAndPassword(
+              emailAddress: state.emailAddress,
+              password: state.password,
+            );
+          }
+          emit(
+            state.copyWith(
+              isSubmitting: false,
+              showErrorMessages: true,
+              authFailureOrSuccessOption: optionOf(failureOrSuccess),
+            ),
+          );
+        },
+        signInWithEmailAndPasswordPressed: (e) async {
+          Either<AuthFailure, Unit>? failureOrSuccess;
+          final isEmailValid = state.emailAddress.isValid();
+          final isPasswordValid = state.password.isValid();
+          if (isEmailValid && isPasswordValid) {
+            emit(
+              state.copyWith(
+                isSubmitting: true,
+                authFailureOrSuccessOption: none(),
+              ),
+            );
+            failureOrSuccess = await _iAuthFacade.signInWithEmailAndPassword(
+              emailAddress: state.emailAddress,
+              password: state.password,
+            );
+          }
+          emit(
+            state.copyWith(
+              isSubmitting: false,
+              showErrorMessages: true,
+              authFailureOrSuccessOption: optionOf(failureOrSuccess),
+            ),
+          );
+        },
+        signInWithGooglePressed: (e) async {
+          emit(
+            state.copyWith(
+              isSubmitting: true,
+              authFailureOrSuccessOption: none(),
+            ),
+          );
+          final failureOrSuccess = await _iAuthFacade.signInWithGoogle();
+          emit(
+            state.copyWith(
+              isSubmitting: false,
+              authFailureOrSuccessOption: some(failureOrSuccess),
+            ),
+          );
+        },
+      );
     });
   }
 }
